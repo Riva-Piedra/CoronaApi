ce6233f102f5274f2ce1ec9539b0a81f154b253e
Riva <68891210+Riva-Piedra@users.noreply.github.com>
2021-04-04 20:17:59 -0300

    Se creo clase "Auth"

--- clases/auth.class.php
+++ clases/auth.class.php
@@ -1,9 +1,46 @@
 <?php
 
 require_once "../conexion/conexion.class.php";
+require_once "response.php";
 
 class Auth extends Conexion {
-    
+
+    private $usuario;
+    private $password;
+    private $token;
+
+    private function login($json){
+        $res = new Response;
+        $data = json_decode($json, true);
+        $usuario = $data['usuario'];
+        $password = $data['password'];
+        $query = "SELECT usuario, pass from usuarios WHERE usuario = '$usuario' AND pass = '$password'";
+        $result = parent::only_fetch_data($query);
+        if($result){
+            $res = $res->code_200();
+            return $res;
+        } else {
+            $res = $res->error_404();
+            return $res;
+        }
+    }
+
+    private function registro($json){
+        $res = new Response;
+        $data = json_decode($json, true);
+        $this->usuario = $data['usuario'];
+        $this->password = $data['pass'];
+        $pass2 = $data['pass2'];
+        $query = "INSERT INTO usuarios(usuario, pass) values('$this->usuario', '$this->password')";
+        $result = parent::alter_data($query);
+        if($result){
+            $res = $res->code_201();
+            return $res;
+        } else {
+            $res = $res->error_404();
+            return $res;
+        }
+    }
 }
 
 
